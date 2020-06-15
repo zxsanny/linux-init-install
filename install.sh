1720d96b5ea4d38b9d2486feed586bc24a2f1f04
zxsanny <zxsanny@gmail.com>
2020-06-15 17:58:22 +0300

    add sudo-s

--- install.sh
+++ install.sh
@@ -4,12 +4,12 @@ sudo apt install dconf-editor -y
 
 #google chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install ./google-chrome-stable_current_amd64.deb -y
+sudo apt install ./google-chrome-stable_current_amd64.deb -y
 rm google-chrome-stable_current_amd64.deb -f
 
 #GO
 wget https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.14.4.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.14.4.linux-amd64.tar.gz
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bash_profile
 source ~/.bash_profile
 rm -rf go
@@ -18,7 +18,7 @@ rm -rf go
 git clone https://github.com/zyedidia/micro
 cd micro
 make build
-mv micro /usr/local/bin
+sudo mv micro /usr/local/bin
 cd ..
 rm -rf micro
 
