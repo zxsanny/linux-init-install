7917ed6f72c3526fb56840dbc5a9f454b2d93235
zxsanny <zxsanny@gmail.com>
2021-05-06 22:42:56 +0300

    add apt-get update, apt install make and gcc

--- install.sh
+++ install.sh
@@ -1,22 +1,17 @@
-sudo apt install xclip wget dconf-editor -y
+sudo apt-get update
+sudo apt install make xclip wget dconf-editor gcc -y
 
 #GO
-if [ !go>/dev/null ];
-then
-	wget https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz
-	sudo tar -C /usr/local -xzf go1.14.4.linux-amd64.tar.gz
-	echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bash_profile
-	source ~/.bash_profile
-	rm -rf go*
-fi
+wget https://dl.google.com/go/go1.14.4.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.14.4.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bash_profile
+source ~/.bash_profile
+rm -rf go*
 
 #micro editor
-if [ !micro>/dev/null ];
-then
-	git clone https://github.com/zyedidia/micro
-	cd micro
-	make build
-	sudo mv micro /usr/local/bin
-	cd ..
-	rm -rf micro
-fi
+git clone https://github.com/zyedidia/micro
+cd micro
+make build
+sudo mv micro /usr/local/bin
+cd ..
+rm -rf micro
